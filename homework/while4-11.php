7ca42b7f055dcd337392d16b79425a1bb656acbe
JeEonPark <ggungtoo@icloud.com>
2021-10-08 21:32:08 +0900

    title 변경

--- homework/while4-11.php
+++ homework/while4-11.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>hello PHP</title>
+        <title>섭씨에서 화씨로 온도 변화</title>
         <style>
         </style>
     </head>
