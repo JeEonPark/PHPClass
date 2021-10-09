9edf55ce9174049f6ae59e6fc85ccce40dc54c7e
JeEonPark <ggungtoo@icloud.com>
2021-10-09 22:51:39 +0900

    title 변경

--- homework/while4-13.php
+++ homework/while4-13.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>hello PHP</title>
+        <title>구구단</title>
         <style>
         </style>
     </head>
