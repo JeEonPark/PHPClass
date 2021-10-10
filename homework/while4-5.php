6d2ee9d26bf56216754b61e4714968617d6d6647
JeEonPark <ggungtoo@icloud.com>
2021-10-10 21:52:12 +0900

    4-5 title 변경

--- homework/while4-5.php
+++ homework/while4-5.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>hello PHP</title>
+        <title>인치 센티미터</title>
         <style>
         </style>
     </head>
