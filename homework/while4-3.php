b73be8449e4a5e72739a26055c0de849a77d2238
JeEonPark <ggungtoo@icloud.com>
2021-10-11 22:10:38 +0900

    title 변경

--- homework/while4-3.php
+++ homework/while4-3.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>hello PHP</title>
+        <title>정수 중 3의 배의 합</title>
         <style>
         </style>
     </head>
