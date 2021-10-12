28cbba3b3424b3322ca90257c72e5e446e992bf8
JeEonPark <ggungtoo@icloud.com>
2021-10-12 14:22:24 +0900

    Session

--- week6_cookie.php
+++ week6_cookie.php
@@ -1,11 +1,26 @@
 <?php
+/* //쿠키
     $a = setcookie("userid", "rubato");
-    $b = setcookie("username", "jeeonpark");
+    $b = setcookie("username", "jeeonpark", time() + 20);
+    setcookie("userid", "", time() - 3600);
 
     if($a and $b) {
         echo "쿠키 생성 완료";
         echo "<br> userid: ".$a."<br>";
         echo "username: ".$b."<br>";
     }
+    */
+
+    //세션
+    session_start();
+
+    $_SESSION["userid"] = "park";
+    $_SESSION["password"] = "1234";
+
+    echo "<br>userid : ".$_SESSION['userid']."<br>";
+    echo "<br>pass : ".$_SESSION['password']."<br>";
+
+    unset($_SESSION["password"]);
+    session_unset();
 
 ?>
--- week6_getcookie.php
+++ week6_getcookie.php
@@ -1,4 +1,5 @@
 <?php
+/*
     if(isset($_COOKIE["userid"])) {
         $userid = $_COOKIE["userid"];
         echo "userid: ".$userid."<br>";
@@ -8,4 +9,16 @@
         $username = $_COOKIE["username"];
         echo "username: ".$username;
     }
+    */
+
+    session_start();
+    if(isset($_SESSION["userid"])){
+        $id = $_SESSION["userid"];
+        echo $id."<br>";
+    }
+    if(isset($_SESSION["password"])){
+        $ps = $_SESSION["password"];
+        echo $ps."<br>";
+    }
+
 ?>
