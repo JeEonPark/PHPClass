730518706d244e5a8ff94433d8ee36fddf87ee83
JeEonPark <ggungtoo@icloud.com>
2021-11-02 15:27:47 +0900

    11.2 server

---
+++ finaltest/id_check.php
@@ -0,0 +1,25 @@
+<h2>아이디 중복 체크</h2>
+
+<?php
+    $id = $_GET["id"];
+
+    if($id) {
+        $con = mysqli_connect("localhost", "root", "root", "sample3");
+        $sql = "select * from logtable where id='$id'";
+        $result = mysqli_query($con, $sql);
+        $num = mysqli_num_rows($result);
+        mysqli_close($con);
+
+        if($num) {
+            //아이디 이미 사용 중
+            echo "이미 사용 중인 아이디 입니다.<br>";
+            echo "다른 아이디를 사용해 주세요.<br>";
+        } else {
+            //아이디 사용 가능
+            echo "사용 가능한 아이디 입니다.<br>";
+        }
+    } else {
+        //아이디 공백
+        echo "ID를 입력해 주세요.<br>";
+    }
+?>
