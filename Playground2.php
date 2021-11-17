224b80ac1c7867af3a1343fbc520084beb25f334
JeEonPark <ggungtoo@icloud.com>
2021-11-18 01:51:55 +0900

    c

--- Playground2.php
+++ Playground2.php
@@ -1,8 +1,8 @@
 <?php
     $bool = $_POST["test"];
     if($bool == 1){
-        $a = setcookie("a", "aaac");
+        $a = setcookie("a", "aa");
     } else {
-        $b = setcookie("b", "bbcb");
+        $b = setcookie("b", "bb");
     }
 ?>
